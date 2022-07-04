a51c1714a04cea94ea9a19113b48a6bde981a719
Chutiphong <chutiphong.charoenchao@gmail.com>
2022-07-04 20:33:09 +0700

    style: :lipstick: change header color

--- components/navigator/Header.tsx
+++ components/navigator/Header.tsx
@@ -2,41 +2,38 @@ export interface IHeader {}
 
 const Header: React.FC<IHeader> = () => {
   return (
-    <header className="h-20 shadow-sm bg-slate-500 flex justify-center items-center bg-gradient-to-r from-green-900 to-green-700 ">
+    <header className="h-20 shadow-sm bg-slate-500 flex justify-center items-center bg-gradient-to-r from-slate-700 to-slate-800 ">
       <nav className="flex justify-between items-center  w-10/12">
-        <div className="block text-3xl text-neutral-50">Logo</div>
+        <div className="block text-3xl text-neutral-50">Diyoor</div>
         <div className="invisible sm:visible">
           <ul className="flex  items-baseline">
             <li className="mr-6 transition-all">
               <a
-                className="text-neutral-50 hover:underline transition-all"
+                className="text-neutral-50 hover:text-slate-400 transition-all"
                 href="#"
               >
-                Link
+                Game
               </a>
             </li>
             <li className="mr-6">
               <a
-                className="text-neutral-50 hover:underline transition-all"
+                className="text-neutral-50 hover:text-slate-400 transition-all"
                 href="#"
               >
-                Link
+                Coffee
               </a>
             </li>
             <li className="mr-6">
               <a
-                className="text-neutral-50 hover:underline transition-all"
+                className="text-neutral-50 hover:text-slate-400 transition-all"
                 href="#"
               >
-                Link
+                Alcohol
               </a>
             </li>
             <li>
-              <button className="text-neutral-50 mr-1 bg-green-800 hover:text-green-900 hover:bg-neutral-50 p-1 px-3 rounded-3xl transition-all">
+              <button className="text-slate-900 mr-1 bg-slate-50 hover:text-slate-50 hover:bg-slate-500 p-1 px-3 rounded-3xl transition-all">
                 Sing In
-              </button>
-              <button className="bg-neutral-50 text-green-900 p-1 px-3 rounded-3xl  active:rounded-3xl transition-all">
-                Sign Up
               </button>
             </li>
           </ul>
